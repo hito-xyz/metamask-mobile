4dad1d5b12f8655a8d79b914602107722c9e9144
Max Fedin <max@optic.xyz>
2023-08-24 14:40:04 +0400

    Add hito wallet support to ConnectNFCAlert.

--- app/components/Views/ConnectHardwareWallets/ConnectNFCAlert.tsx
+++ app/components/Views/ConnectHardwareWallets/ConnectNFCAlert.tsx
@@ -1,23 +1,23 @@
-import React, {
-  Fragment,
-  useCallback,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react';
-import { Modal, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import AnimatedQRScannerModal from '../../UI/QRHardware/AnimatedQRScanner';
+import React, { Fragment, useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import BlockingActionModal from '../../UI/BlockingActionModal';
-import { strings } from '../../../../locales/i18n';
-import Alert, { AlertType } from '../../Base/Alert';
 import { fontStyles } from '../../../styles/common';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import Device from '../../../util/device';
 import { useTheme } from '../../../util/theme';
-import HitoInstruction from './HitoInstruction';
 import { ConfirmTransactionModal, ReadyToScanModal } from './modals';
-import Button, { ButtonSize, ButtonVariants, ButtonWidthTypes } from '../../../component-library/components/Buttons/Button';
+import Button, {
+  ButtonSize,
+  ButtonVariants,
+  ButtonWidthTypes,
+} from '../../../component-library/components/Buttons/Button';
+import { HitoSDKController } from '@hito-wallet/hito-react-native-sdk/index';
 
 interface IConnectHitoWalletViewProps {
   navigation: any;
@@ -63,22 +63,65 @@ const createStyles = (colors: any) =>
       fontSize: 14,
       ...fontStyles.normal,
     },
+    button: {
+      marginTop: 20,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
   });
 
 const ConnectNFCAlert = ({ navigation }: IConnectHitoWalletViewProps) => {
   const { colors } = useTheme();
   const styles = createStyles(colors);
+
+  const ETH_ADDRESS_TEST = '0xF91d6da14A3d18e22adD79f8BF0A8C4D852f8b84';
+  const UNSIGNED_TRANSACTION_TEST =
+    '0xee818d840b766ae082520894feed146aa5f20bc991a994a1ac2fe0bb75df2bb087038d7ea4c680008083aa36a78080';
+  const hitoNFCController = new HitoSDKController();
+  const [addressToTransmit, setAddressToTransmit] = useState(ETH_ADDRESS_TEST);
+  const [unsignedTXToTransmit, setUnsignedTXToTransmit] = useState(
+    UNSIGNED_TRANSACTION_TEST,
+  );
+  const [signedTXData, setTxData] = useState('');
   const [modalNFCVisible, setModalNFCVisible] = useState(false);
   const [modalConfirmVisible, setModalConfirmVisible] = useState(false);
-  
+
+  useEffect(() => {
+    hitoNFCController.initNFC();
+  });
+
+  const handleTransmitUnsigned = async () => {
+    await hitoNFCController.transmitUnsignedEVMTransaction(
+      addressToTransmit,
+      unsignedTXToTransmit,
+    );
+  };
+
+  const handleScanSigned = async () => {
+    try {
+      const signedData = await hitoNFCController.scanSignedEVMTransaction();
+      if (signedData) {
+        setTxData(JSON.stringify(signedData));
+      }
+    } catch (error) {
+      console.error('Error reading NFC:', error);
+    }
+  };
   return (
     <Fragment>
       <View style={styles.container}>
-        <ReadyToScanModal opened={modalNFCVisible} close={() => setModalNFCVisible(false)} />
-        <ConfirmTransactionModal opened={modalConfirmVisible} close={() => setModalConfirmVisible(false)} />
+        <ReadyToScanModal
+          opened={modalNFCVisible}
+          close={() => setModalNFCVisible(false)}
+        />
+        <ConfirmTransactionModal
+          opened={modalConfirmVisible}
+          close={() => setModalConfirmVisible(false)}
+        />
         <View style={styles.header}>
           <Icon
-            name='qrcode'
+            name="qrcode"
             size={42}
             style={styles.qrcode}
             color={colors.text.default}
@@ -90,8 +133,7 @@ const ConnectNFCAlert = ({ navigation }: IConnectHitoWalletViewProps) => {
             <MaterialIcon name="close" size={15} style={styles.closeIcon} />
           </TouchableOpacity>
         </View>
-
-        <View style={{ marginTop: 20, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <View style={styles.button}>
           <Button
             variant={ButtonVariants.Primary}
             label={'Open NFC connection'}
@@ -100,12 +142,66 @@ const ConnectNFCAlert = ({ navigation }: IConnectHitoWalletViewProps) => {
             width={ButtonWidthTypes.Auto}
           />
         </View>
-        <View style={{ marginTop: 20, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <View style={styles.button}>
           <Button
             variant={ButtonVariants.Primary}
             label={'Open Confirm'}
             size={ButtonSize.Md}
             onPress={() => setModalConfirmVisible(true)}
+            width={ButtonWidthTypes.Auto}
+            isDanger
+          />
+        </View>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.text}>Transmit Transaction</Text>
+        <Text style={styles.text}>Address</Text>
+        <TextInput
+          value={addressToTransmit}
+          onChangeText={setAddressToTransmit}
+          placeholder="Enter address"
+          multiline
+          numberOfLines={1}
+          maxLength={42}
+        />
+        <Text style={styles.text}>Unsigned transaction</Text>
+        <TextInput
+          value={unsignedTXToTransmit}
+          onChangeText={setUnsignedTXToTransmit}
+          placeholder="Enter unsigned transaction"
+          multiline
+          numberOfLines={4}
+          maxLength={200}
+        />
+        <View style={styles.button}>
+          <Button
+            variant={ButtonVariants.Primary}
+            label={'Transmit Unsigned Transaction'}
+            size={ButtonSize.Md}
+            onPress={handleTransmitUnsigned}
+            width={ButtonWidthTypes.Auto}
+          />
+        </View>
+
+        <Text style={styles.text}>Scan Transaction</Text>
+        <Text style={styles.text}>
+          {signedTXData ? `Transaction Data: ${signedTXData}` : ''}
+        </Text>
+        <View style={styles.button}>
+          <Button
+            variant={ButtonVariants.Primary}
+            label={'Scan Signed Transaction'}
+            size={ButtonSize.Md}
+            onPress={handleScanSigned}
+            width={ButtonWidthTypes.Auto}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            variant={ButtonVariants.Primary}
+            label={'Clear'}
+            size={ButtonSize.Md}
+            onPress={() => setTxData('')}
             width={ButtonWidthTypes.Auto}
             isDanger
           />
